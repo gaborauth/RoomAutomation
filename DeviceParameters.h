4d5620f84855ce4a9eb12ea06d1b2eb91c3cb8ef
gaborauth <auth.gabor@gmail.com>
2018-04-27 16:51:23 +0200

    Added node parameters

--- DeviceParameters.h
+++ DeviceParameters.h
@@ -2,9 +2,11 @@
  * Device parameters.
  */
 #define NUMBER_OF_DEVICES 1
+#define NUMBER_OF_NODES 1
 
 typedef struct {
      String name;
+     String deviceId;
 
      int pinLayout;
 
@@ -21,7 +23,18 @@ typedef struct {
      int staticMask[4];
 } deviceParameters;
 
+typedef struct {
+     String name;
+     String nodeId;
+     String nodeKey;
+     String deviceId;
+} nodeParameters;
+
 deviceParameters deviceParametersMap[NUMBER_OF_DEVICES] = {
-    {"000000000000", 0, 0, 0, 0, 0, 0, 0, 0, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, // Test-1
+    {"test", "61d315e0-46c7-11e8-a240-7bca10e5bd5b", 0, 0, 0, 0, 0, 0, 0, 0, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, // Test-1
+};
+
+nodeParameters nodeParametersMap[NUMBER_OF_NODES] = {
+    {"test", "97ac1190-1094-11e7-bb99-dd93c17d8a4d", "mdXaRa6IbdJODwy3yolFKQ", "61d315e0-46c7-11e8-a240-7bca10e5bd5b"}, // Test-1
 };
 
