9855944d32326499e87320f02933e8c067b85981
gaborauth <auth.gabor@gmail.com>
2018-04-27 16:48:28 +0200

    Modified IOT URL

--- WiFiParameters.h
+++ WiFiParameters.h
@@ -5,7 +5,7 @@
 #define AP2_SSID       "ap03"
 #define AP2_PASSWORD   "********"
 
-#define IOT_BASE_URL   "http://iothub.live/rest"
+#define IOT_BASE_URL   "http://api.iothub.live"
 #define IOT_USER_ID    "11cf8f40-460b-11e8-b506-778c45541df0"
 #define IOT_PASSWORD   "vdxDdz3DX9rkByNn"
 
