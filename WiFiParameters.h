f04ae727cc74de605bb72cc61b6cb2c0b73eb098
gaborauth <auth.gabor@gmail.com>
2018-05-12 10:36:56 +0200

    Added comment

--- WiFiParameters.h
+++ WiFiParameters.h
@@ -5,6 +5,9 @@
 #define AP2_SSID       "ap03"
 #define AP2_PASSWORD   "********"
 
+/**
+ * Credentials of a demo account, you must be change it.
+ */
 #define IOT_BASE_URL   "http://api.iothub.live"
 #define IOT_USER_ID    "11cf8f40-460b-11e8-b506-778c45541df0"
 #define IOT_PASSWORD   "vdxDdz3DX9rkByNn"
